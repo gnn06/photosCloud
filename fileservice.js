5902fcf8fca7cd6757221a18e9edb2651def2a12
gilles orsini <gilles.orsini@gmail.com>
2017-06-21 22:38:48 +0200

    finalisation fileservice avec unit test, utilisation chai, chai-fs installation eslint

--- fileservice.js
+++ fileservice.js
@@ -2,35 +2,50 @@
 
 var fs = require('fs');
 
-exports.walk = function (rootFolder, folder) {
-  // Récupérer ancien contenu
-  if (fs.existsSync(rootFolder + '/' + folder + '/folder.json')) {
-    var content = fs.readFileSync(rootFolder + '/' + folder + '/folder.json', 'utf-8');
-    return JSON.parse(content);
-  }
-  // parcourir pour soit créer le contenu soit récupérer les sous-contenus
-  // On isole le contenu courant des sous-contenu pour pouvoir stocker le contenu courant tout seul.
-  var result = [];
-  var extensionsToRetrieve = ['jpg', 'mp4'];
-  var files = fs.readdirSync (rootFolder + '/' + folder, "utf-8");
-  for (var i = 0; i < files.length; i++) {
-    var stat = fs.statSync(rootFolder + '/' + folder + '/' + files[i]);
-    if (stat.isDirectory()) {
-      var subResult = this.walk(rootFolder, folder + '/' + files[i]);
-	  // adapte les urls des sous-contenus 
-      result = result.concat(subResult);
-    } else {
-	  // Ne faire la récupération que si le contenu n'a pas été préalablement récupéré	
-      var extension = files[i].substr(-3).toLowerCase();
-      if (extensionsToRetrieve.indexOf(extension) > -1) {
-        var item = {};
-        item.url = folder + '/' + files[i];
-        result.push(item);
-      }
-    }
-  }
-  // stocker le contenu du répertoire courant si on vient de le construire (et pas les sous-contenu)
-  var content = JSON.stringify(result);
-  fs.writeFileSync(rootFolder + '/' + folder + '/folder.json', content, 'utf-8');
-  return result;
-}
+const extensionsToRetrieve = ['jpg', 'mp4'];
+
+exports.walk = function (folder, options) {
+	var currentContent = [];
+	var allSubContent = [];
+	var currentContentFromCache = false;
+	var flattenContent = [];
+
+	// Récupérer ancien contenu
+	if (fs.existsSync(options.photoFolder + '/' + folder + '/folder.json')) {
+		let content = fs.readFileSync(options.photoFolder + '/' + folder + '/folder.json', 'utf-8');
+		currentContent = JSON.parse(content);
+		currentContentFromCache = currentContent.length > 0;
+	}
+	// parcourir pour soit créer le contenu soit récupérer les sous-contenus
+	// On isole le contenu courant des sous-contenu pour pouvoir stocker le contenu courant tout seul.
+	var files = fs.readdirSync (options.photoFolder + '/' + folder, 'utf-8');
+	for (var i = 0; i < files.length; i++) {
+		var stat = fs.statSync(options.photoFolder + '/' + folder + '/' + files[i]);
+		if (stat.isDirectory()) {
+			var subResult = this.walk(folder + '/' + files[i], options);
+			// adapte les urls des sous-contenus
+			allSubContent = allSubContent.concat(
+				subResult.map(item => {
+					item.url = files[i] + '/' + item.url;
+					return item;
+				}));
+		} else if (!currentContentFromCache) {
+			// Ne faire la récupération que si le contenu n'a pas été préalablement récupéré
+			var extension = files[i].substr(-3).toLowerCase();
+			if (extensionsToRetrieve.indexOf(extension) > -1) {
+				var item = {};
+				item.url = files[i];
+				currentContent.push(item);
+			}
+		}
+	}
+	// stocker le contenu du répertoire courant si on vient de le construire (et pas les sous-contenu)
+	if (!currentContentFromCache) {
+		let content = JSON.stringify(currentContent);
+		fs.writeFileSync(options.dataFolder + '/' + folder + '/folder.json', content, 'utf-8');
+	}
+	// fusionne contenu courant et sous-contenus
+	flattenContent = currentContent.concat(allSubContent);
+	// console.log('current=',currentContent.length,' allSubContent=',allSubContent.length,'flattenContent=',flattenContent.length);
+	return flattenContent;
+};
--- test/fileservice.test.js
+++ test/fileservice.test.js
@@ -2,41 +2,68 @@
 
 var service = require('../fileservice.js');
 var fs = require('fs');
-var assert = require('assert');
+var chai = require('chai');
+var chaiFiles = require('chai-files');
+
+chai.use(chaiFiles);
+
+var assert = chai.assert;
+var expect = chai.expect;
+var file = chaiFiles.file;
+var dir = chaiFiles.dir;
+
+function deleteFile (filename) {
+	if (fs.existsSync(filename)) {
+			fs.unlinkSync(filename);
+	}
+}
 
 describe('fileservice', function() {
   describe('read', function () {
-    it('sould returns array, only jpg, case insensitive', function () {
-      var result = service.walk('E:/temp/dev/mochetest/test1', '');
+    it('should returns array, only jpg, case insensitive, store into <data> Folder', function () {
+			deleteFile('E:/temp/dev/mochetest/test1/data/folder.json');
+			deleteFile('E:/temp/dev/mochetest/test1/photo/folder.json');
+      var result = service.walk('', { photoFolder : 'E:/temp/dev/mochetest/test1/photo', dataFolder : 'E:/temp/dev/mochetest/test1/data' });
       var expected = [
-        { url : '/file1.jpg' },
-        { url : '/file2.JPG' },
-        { url : '/file4.mp4'}
+        { url : 'file1.jpg' },
+        { url : 'file2.JPG' },
+        { url : 'file4.mp4'}
       ];
       assert.deepEqual(result, expected);
+			expect(file('E:/temp/dev/mochetest/test1/photo/folder.json')).to.not.exist;
+			expect(file('E:/temp/dev/mochetest/test1/data/folder.json')).to.exist;
     });
 
     it('should returns subfolder', function () {
-      var result = service.walk('E:/temp/dev/mochetest/test2', '');
+			delete('E:/temp/dev/mochetest/test2/folder.json');
+			delete('E:/temp/dev/mochetest/test2/subfolder/folder.json');
+      var result = service.walk('', { photoFolder : 'E:/temp/dev/mochetest/test2', dataFolder : 'E:/temp/dev/mochetest/test2' });
       var expected = [
-        { url : '/parentfile.jpg'},
-        { url : '/subfolder/subfile.jpg' }
+        { url : 'parentfile.jpg'},
+        { url : 'subfolder/subfile.jpg' }
       ];
       assert.deepEqual(result, expected);
     });
 
-    it('should store result', function () {
+    it('should store result in current and also in subfolder, level by level', function () {
       if (fs.existsSync('E:/temp/dev/mochetest/test2/folder.json')) {
           fs.unlinkSync('E:/temp/dev/mochetest/test2/folder.json');
       }
       if (fs.existsSync('E:/temp/dev/mochetest/test2/subfolder/folder.json')) {
           fs.unlinkSync('E:/temp/dev/mochetest/test2/subfolder/folder.json');
       }
-      var result = service.walk('E:/temp/dev/mochetest/test2', '');
+
+      var result = service.walk('', { photoFolder : 'E:/temp/dev/mochetest/test2', dataFolder : 'E:/temp/dev/mochetest/test2' });
+
       var existPost = fs.existsSync('E:/temp/dev/mochetest/test2/folder.json');
       assert.ok(existPost, 'fichier non crée après appel');
-	  var existPostSub = fs.existsSync('E:/temp/dev/mochetest/test2/subfolder/folder.json');
-	  assert.ok(existPost, 'sous fichier non crée après appel');
+			var existPostSub = fs.existsSync('E:/temp/dev/mochetest/test2/subfolder/folder.json');
+  	  assert.ok(existPost, 'sous fichier non crée après appel');
+      expect(file('E:/temp/dev/mochetest/test2/folder.json')).to.contain('parentfile');
+      expect(file('E:/temp/dev/mochetest/test2/folder.json')).to.not.contain('subfile');
+  	  expect(file('E:/temp/dev/mochetest/test2/subfolder/folder.json')).to.not.contain('parentfile');
+
+			expect(file('E:/temp/dev/mochetest/test2/subfolder/folder.json')).to.contain('subfile');
     });
 
     it('should use stored data', function () {
@@ -45,13 +72,15 @@ describe('fileservice', function() {
       var fileNotExist = fs.existsSync('E:/temp/dev/mochetest/test3/file.jpg');
       assert.ok(!fileNotExist);
       var result = [];
-      result = service.walk('E:/temp/dev/mochetest/test3', '');
+      result = service.walk('', { photoFolder : 'E:/temp/dev/mochetest/test3', dataFolder : 'E:/temp/dev/mochetest/test3' });
       assert.deepEqual(result, [{url:'file.jpg'}]);
     });
-	
-	it('should not store absolute url');
-	
-	it('should return relative path including root');
+
+		it('should not store absolute url', function () {
+			expect(file('E:/temp/dev/mochetest/test2/folder.json')).to.not.contain('E:');
+			expect(file('E:/temp/dev/mochetest/test2/folder.json')).to.not.contain('test2');
+			expect(file('E:/temp/dev/mochetest/test2/subfolder/folder.json')).to.not.contain('subfolder');
+		});
   })
 })
 
