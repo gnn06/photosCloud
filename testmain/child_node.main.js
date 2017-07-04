30a1cd214542872a970feb840023cc2232d38736
gilles orsini <gilles.orsini@gmail.com>
2017-07-04 21:37:11 +0200

    finalisation test sur / \

--- testmain/child_node.main.js
+++ testmain/child_node.main.js
@@ -3,11 +3,15 @@ const proc = require('child_process');
 function testChildNode () {
 
     var argv = [];
-    // argv = argv.concat('c:\\temp\\dir space\\file.txt');
     argv = argv.concat('/r');
-    argv = argv.concat('c:/temp/dir space/file.txt'); // OK
-    // argv = argv.concat('c:\\temp\\dir space\\file.txt'); // OK
-    // argv = argv.concat('"c:\\temp\\dir space\\file.txt"'); // KO
+    // argv = argv.concat('-r');
+    // argv = argv.concat('e:/temp/dir space/file.txt');       // OK
+    // argv = argv.concat('e:\\temp\\dir space\\file.txt');    // OK
+    // argv = argv.concat('"e:\\temp\\dir space\\file.txt"');  // KO
+
+    // argv = argv.concat('/tmp/dir space/file.txt');          // OK
+    // argv = argv.concat('\\tmp\\dir space\\file.txt');       // OK
+    // argv = argv.concat('"e:\\temp\\dir space\\file.txt"');  // KO
 
     proc.execFile('sort', argv, (error, stdout, stderr) => {
         if (error) {
@@ -16,10 +20,22 @@ function testChildNode () {
         console.log(stdout);
     });
 
-    // proc.exec('sort /r "c:\\temp\\dir space\\file.txt"', (error, stdout, stderr) => {
-    //     // 'sort /r c:/temp/dir space/file.txt'      => KO Command failed: sort /r c:/temp/dir space/file.txt
-    //     // 'sort /r c:\\temp\\dir space\\file.txt'   => KO Command failed: sort /r c:\temp\dir space\file.txt
-    //     // 'sort /r "c:\\temp\\dir space\\file.txt"' => OK
+    // proc.exec(
+        // 'sort /r e:/temp/dir space/file.txt'      //      => KO Command failed: sort /r c:\temp\dir space\file.txt
+        // 'sort /r e:/temp/dir\ space/file.txt'      //      => KO Command failed: sort /r c:\temp\dir space\file.txt
+        // 'sort /r e:/temp/dir\\ space/file.txt'      //      => KO Command failed: sort /r c:\temp\dir space\file.txt
+        // 'sort /r e:\\temp\\dir space\\file.txt'      //      => KO Command failed: sort /r c:\temp\dir space\file.txt
+        // 'sort /r e:\\temp\\dir\ space\\file.txt'      //      => KO
+        // 'sort /r e:\\temp\\dir\\ space\\file.txt'      //      => KO
+        // 'sort /r "e:\temp\dir space\file.txt"'        //      => KO
+        // 'sort /r "e:\\temp\\dir space\\file.txt"'    //      => OK
+
+        // 'sort -r /tmp/dir space/file.txt'         //      => KO Command failed: sort /r c:/temp/dir space/file.txt
+        // 'sort -r /tmp/dir\ space/file.txt'        //      => KO
+        // 'sort -r /tmp/dir\\ space/file.txt'       //      => OK
+        // 'sort -r \\tmp\\dir space\\file.txt'      //      => KO Command failed: sort /r c:\temp\dir space\file.txt
+        // 'sort -r "/tmp/dir space/file.txt"'       //      => OK
+    //     ,(error, stdout, stderr) => {
     //     if (error) {
     //         console.error(error);
     //     }
