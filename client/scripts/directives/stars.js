dd96818e31daf82332b70616104f8deaf106a817
gilles orsini <gilles.orsini@gmail.com>
2017-10-04 21:28:53 +0200

    bug : watch APRES récupération pour éviter de faire une sauvergarde de la valeur qu'on vient de récupérer.

--- client/scripts/directives/stars.js
+++ client/scripts/directives/stars.js
@@ -6,35 +6,36 @@ module.directive('pcStars', function () {
 
 	return {
 		templateUrl: 'templates/stars.html',
-        replace: true,
-                    
-        controller: function ($scope, $http, $routeParams) {
+		replace: true,
+					
+		controller: function ($scope, $http, $routeParams) {
 			console.log($routeParams);
 
 			$http({method: 'GET',
 				url: '/data/' + $routeParams.folder
 				/*, cache: $templateCache*/}).
-			then(function(response) {
-				console.log('data retrieved', response.data);
-				$scope.stars = response.data.stars;
-			}, function(response) {
-				console.error('error');
-			});
-
-			$scope.$watch('stars', function (newValue, oldValue) {
-				if (newValue != oldValue) {
-					console.log('post');
-					$http({method: 'POST',
-						url: '/data/' + $routeParams.folder,
-						data: { stars: $scope.stars }
-						}).
-					then(function(response) {
-						console.log('saved');
-					}, function(response) {
-						console.error('error');
+				then(function(response) {
+					console.log('data retrieved', response.data);
+					$scope.stars = response.data.stars;
+					$scope.$watch('stars', function (newValue, oldValue) {
+						if (newValue != oldValue) {
+							console.log('post');
+							$http({method: 'POST',
+								url: '/data/' + $routeParams.folder,
+								data: { stars: $scope.stars }
+							}).
+								then(function(response) {
+									console.log('saved');
+								}, function(response) {
+									console.error('error');
+								});
+						}
 					});
-				}
-			});
+				}, function(response) {
+					console.error('error');
+				});
+
+			
 
 		}
 	};
