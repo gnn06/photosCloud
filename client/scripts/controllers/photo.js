344fad82c0dbacb6b9645d78215f7157bc5945cc
gilles orsini <gilles.orsini@gmail.com>
2017-10-19 21:12:30 +0200

    refactor : eslint

--- client/scripts/controllers/photo.js
+++ client/scripts/controllers/photo.js
@@ -10,57 +10,50 @@
 angular.module('photosAngularApp')
 	.controller('PhotoCtrl', function ($scope, $rootScope, $location, $route, $routeParams, $http, 
 		$filter, photoservice) {
-	console.log("dans PhotoCtrl")
+		console.log('dans PhotoCtrl');
 	
-	if (!$rootScope.photos) {
-		$http({method: 'GET',
-		  url: '/thumbnails'
-		  /*, cache: $templateCache*/}).
-		then(function(response) {
-			$rootScope.photos = $scope.photos = response.data;
-			$rootScope.photos.sort((a, b) => {
-				if (a.date < b.date) {
-					return 1;
-				} else if (a.date > b.date) {
-					return -1;
-				} else {
-					return 0;
-				}
-			});
+		if (!$rootScope.photos) {
+			$http({method: 'GET',
+				url: '/thumbnails'
+				/*, cache: $templateCache*/}).
+				then(function(response) {
+					$rootScope.photos = $scope.photos = response.data;
+					$rootScope.photos.sort((a, b) => {
+						if (a.date < b.date) {
+							return 1;
+						} else if (a.date > b.date) {
+							return -1;
+						} else {
+							return 0;
+						}
+					});
+					$scope.currentPhoto = $rootScope.photos
+						.map(function(item){return decodeURI(item.url)})
+						.indexOf('/thumbnail/' + $routeParams.folder);
+				}, function(response) {
+					console.error('error');
+				});		
+		} else {
 			$scope.currentPhoto = $rootScope.photos
-				.map(function(item){return decodeURI(item.url)})
-					.indexOf("/thumbnail/" + $routeParams.folder);
-		}, function(response) {
-			console.error('error');
-		});		
-	} else {
-		$scope.currentPhoto = $rootScope.photos
-			.map(function(item){return decodeURI(item.url)})
-				.indexOf("/thumbnail/" + $routeParams.folder)
-	}
+				.map(function(item){return decodeURI(item.url);})
+				.indexOf('/thumbnail/' + $routeParams.folder);
+		}
 
-	$scope.$location = $location;
-	$scope.$route = $route;
-	$scope.$routeParams = $routeParams;
+		$scope.$location = $location;
+		$scope.$route = $route;
+		$scope.$routeParams = $routeParams;
 
-	var host = $location.$$absUrl.substr(0,$location.$$absUrl.indexOf("#!"));
-
-	$scope.post = function() {
-		
-	};
-
-	$scope.deletePhoto = function(ev) {
-		console.log('delete');
-		var photosIdx = [];
-		photosIdx.push($rootScope.photos[$scope.currentPhoto].url);
-		photoservice.delete(photosIdx, ev)
-		.then(function () {
-			// currentPhoto target next as array was reduced.
-			var next = $rootScope.photos[$scope.currentPhoto].url;
-			$location.path($filter('thumbnail')(next));
-		}, function () {
-			console.log('rejected');
-		});
-
-	};		
-});
+		$scope.deletePhoto = function(ev) {
+			console.log('delete');
+			var photosIdx = [];
+			photosIdx.push($rootScope.photos[$scope.currentPhoto].url);
+			photoservice.delete(photosIdx, ev)
+				.then(function () {
+					// currentPhoto target next as array was reduced.
+					var next = $rootScope.photos[$scope.currentPhoto].url;
+					$location.path($filter('thumbnail')(next));
+				}, function () {
+					console.log('rejected');
+				});
+		};		
+	});
