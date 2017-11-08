194598067a824d5e876777e47ab3f9dbc76bbdf5
gilles orsini <gilles.orsini@gmail.com>
2017-11-08 10:04:50 +0100

    deploy.sh avec variable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-tar --exclude=./client/bower_components --exclude=./src/config.js -zcvf "release/photosCloud-$(date '+%y-%m-%d-%H-%M-%S').tar.gz" ./src/ ./client/ ./package.json
+archive="release/photosCloud-$(date '+%y-%m-%d-%H-%M-%S').tar.gz"
+tar --exclude=./client/bower_components --exclude=./src/config.js -zcvf $archive ./src/ ./client/ ./package.json
 cd /var/photos/photosCloud
-tar -zxvf ~/dev/photosCloud/release/photosCloud-17-10-01-22-09-32.tar.gz
+tar -zxvf ~/dev/photosCloud/$archive
 sudo systemctl restart photosCloud.service
