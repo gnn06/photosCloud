bda55eb02e04d0a12c4bcbcaf3ac9f80f78ddeaa
gilles orsini <gilles.orsini@gmail.com>
2017-11-12 21:39:59 +0100

    refactor : private function of img-resize directive use closure

--- client/scripts/directives/pc-imgresize.js
+++ client/scripts/directives/pc-imgresize.js
@@ -1,27 +1,32 @@
 /* global angular */
 'use strict';
 
+/* 
+ * meilleure solution qu'une div avec background car la taille 
+ * de la div et respecte pas la taille de l'image
+ */
+
 angular.module('photosAngularApp')
-	.directive('pcImgresize', function ($window) {
-		function chooseWidthOrHeight (element) {
-			var w = element[0].width;
-			var h = element[0].height;
-			var ww = $window.innerWidth;
-			var wh = $window.innerHeight;
-			var ratio_W = (w / ww);
-			var ratio_H = (h / wh);
-			if (ratio_W / ratio_H > 1 ) {
-				console.log('resize image => choose width');
-				element.css('width', ww + 'px');
-				element.css('height', '');
-			} else {
-				console.log('resize image => choose height');
-				element.css('height', wh + 'px');
-				element.css('width', '');
-			}
-		}
+	.directive('pcImgresize', function ($window) {		
 		return {
 			link : function (scope, element, attrs) {
+				function chooseWidthOrHeight () {
+					var w = element[0].width;
+					var h = element[0].height;
+					var ww = $window.innerWidth;
+					var wh = $window.innerHeight;
+					var ratio_W = (w / ww);
+					var ratio_H = (h / wh);
+					if (ratio_W / ratio_H > 1 ) {
+						console.log('resize image => choose width');
+						element.css('width', ww + 'px');
+						element.css('height', '');
+					} else {
+						console.log('resize image => choose height');
+						element.css('height', wh + 'px');
+						element.css('width', '');
+					}
+				}
 				// $observe needed as img.src need to be interpolated
 				// to replace {{}}. 
 				attrs.$observe('src', function(newVal, oldVal){
