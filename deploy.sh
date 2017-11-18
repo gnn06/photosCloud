6521711a47a46b1814608c17273aafee6cf7d290
gilles orsini <gilles.orsini@gmail.com>
2017-11-18 22:55:05 +0100

    rajout commande pour update les packages lors du déploiement. NON TESTE

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,7 @@ archive="release/photosCloud-$(date '+%y-%m-%d-%H-%M-%S').tar.gz"
 tar --exclude=./client/bower_components --exclude=./src/config.js -zcvf $archive ./src/ ./client/ ./package.json
 cd /var/photos/photosCloud
 tar -zxvf ~/dev/photosCloud/$archive
+npm install
+cd client
+bower install
 sudo systemctl restart photosCloud.service
