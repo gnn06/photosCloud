3b6406f403c16b52edce0ec20fb68a547f4c7e1b
gilles orsini <gilles.orsini@gmail.com>
2018-01-06 13:32:26 +0100

    bug : script deploiement gérer mode production lors du npm install

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ archive="binaries/photosCloud-$(date '+%y-%m-%d-%H-%M-%S').tar.gz"
 tar --exclude=./client/bower_components --exclude=./node_modules --exclude=./src/config.js -zcvf $archive ./src/ ./client/ ./package.json
 cd /var/photos/application
 tar -zxvf ~/dev/photosCloud/$archive
-npm install
+npm install --production
 cd client
 bower install
 pm2 restart photoscloud
