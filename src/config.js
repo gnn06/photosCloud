78f84a96632541f43fb685ef805777cdcac3f6d2
gnn06 <gilles.orsini@gmail.com>
2018-01-10 21:34:30 +0100

    bug : video mal centrée

--- src/config.js
+++ src/config.js
@@ -1,14 +1,23 @@
-var config = {}
+var config = {};
 
 // absolute path need to express.js module
 
 // DEVELOPPEMENT
 
+// PI
 // const ROOT_FOLDER = '/home/pi/dev/photosCloud/real_sample/';
 // const ROOT_FOLDER = '//raspberrypi/pi/dev/photosCloud/short_sample/';
-// const ROOT_FOLDER = '//raspberrypi/pi/dev/photosCloud/real_sample/';
+// const ROOT_FOLDER = '/home/pi/dev/photosCloud/real_sample/';
 
- const ROOT_FOLDER = 'C:/Users/gorsini/Documents/workspace/photosCloud/real_sample/';
+// WINDOWS
+const ROOT_FOLDER = 'y:/dev/photosCloud/real_sample/';
+// const ROOT_FOLDER = '//raspberrypi/pi/dev/photosCloud/real_sample/';
+// const ROOT_FOLDER = '//raspberrypi/pi/dev/photosCloud/short_sample/';
+
+// const ROOT_FOLDER = '//raspberrypi/PiPhotos/'; // WINDOWS, galerie complete
+
+// const ROOT_FOLDER = './real_sample/';
+// const ROOT_FOLDER = 'C:/Users/gorsini/Documents/workspace/photosCloud/real_sample/';
 
 
 // à terminer par un slash
@@ -19,6 +28,6 @@ config.dataPath      = ROOT_FOLDER + "data/";
 config.trashPath     = ROOT_FOLDER + "corbeille/";
 config.conv_prog     = "convert";
 config.video_prog    = 'avconv';
-config.port          = 8089;
+config.port          = 8090; // 8089 n'est pas accessible depuis proxy docapost
 
 module.exports = config
